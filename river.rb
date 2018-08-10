3ad0522d7bea7c8507d602adc88677ff954a8884
sthai1721 <sophia.thai@rutgers.edu>
2018-08-10 15:01:15 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -38,11 +38,11 @@ class River
   end
 
   def flood(action)
-       action=(@discharge*1.30)
+       @action=(@discharge*1.30)
   end
   
   def dry_up(action)
-       action=(@discharge*0.50)
+       @action=(@discharge*0.50)
   end
   
   
